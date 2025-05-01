53e01f4b25d06fa9c9409801cb8e875e565904f7
thilakreddy4304 <thilakreddy4304@gmail.com>
2025-04-30 23:31:28 -0400

    Add 3D Model

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Button = ({className, id, text}) => {
+  return (
+    <a className = { `${className ?? ''} cta-wrapper` } >
+      <div className='cta-button group'>
+        <div className='bg-circle'/>
+        <p className='text'>
+          {text}
+        </p>
+        <div className='arrow-wrapper'>
+          <img src="/images/arrow-down.svg" alt="arrow" />
+        </div>
+      </div>
+    </a>
+
+  )
+}
+
+export default Button
--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { words } from '../constants/index.js'
+import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
 
-const hero = () => {
+const Hero = () => {
   return (
     <section id="hero" className='relative overflow-hidden'>
         <div className='absolute top-0 left-0z-10'>
@@ -9,6 +11,7 @@ const hero = () => {
       </div>
 
       <div className='hero-layout'>
+        {/* { left side hero content} */}
         <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
            <div className='flex flex-col gap-7'>
             <div className="hero-text">
@@ -28,11 +31,23 @@ const hero = () => {
                 <h1>into Real Projects</h1>
                 <h1> that Deliver Results</h1>
             </div>
+            <p className='text-white-50 md:text-xl relative z-10 pointer-events-none' >
+                 Hi, I'm Thilak, a Software Engineer based in United States <br/> with a passion for code and building products.
+            </p>
+            <Button className='md:w-80 md:h-16 w-60 h-12' id='button' text='See My Work'> 
+            </Button>
            </div>
         </header>
+
+        {/* { right side 3D Model} */}
+        <figure>    
+            <div className='hero-3d-layout'>
+                <HeroExperience />
+            </div>
+        </figure>
       </div>
     </section>
   )
 }
 
-export default hero
+export default Hero
